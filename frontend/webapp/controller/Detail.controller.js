f4ff5789f6d540cd76a1c94e58981dce491b4b3e
Maurizio Moraca <maurizio.moraca@capgemini.com>
2025-03-06 09:54:17 +0000

    Added route to address data creation

--- frontend/webapp/controller/Detail.controller.js
+++ frontend/webapp/controller/Detail.controller.js
@@ -12,11 +12,13 @@ sap.ui.define([
             // read msg from i18n model
             oBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
             var oRouter = this.getOwnerComponent().getRouter();
-            oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
+            oRouter.getRoute("detail").attachPatternMatched(this._onDetailMatched, this);
+            oRouter.getRoute("create").attachPatternMatched(this._onCreateMatched, this);
         },
 
 
-        _onObjectMatched: async function (oEvent) {
+        _onDetailMatched: function (oEvent) {
+            console.log("detail");
             this._vpid = oEvent.getParameter("arguments").vpid;
             this._vctext = oEvent.getParameter("arguments").vctext;
             this._werks = oEvent.getParameter("arguments").werks;
@@ -25,6 +27,10 @@ sap.ui.define([
             this._spart = oEvent.getParameter("arguments").spart;
 
             this._fetchData(baseManifestUrl + `/girovisiteService/Header(vpid='${this._vpid}',vctext='${this._vctext}',werks='${this._werks}',vkorg='${this._vkorg}',vtweg='${this._vtweg}',spart='${this._spart}')?$expand=details`)
+        },
+
+        _onCreateMatched: function (oEvent) {
+            console.log("create");
         },
 
         _fetchData: function (sUrl) {  
